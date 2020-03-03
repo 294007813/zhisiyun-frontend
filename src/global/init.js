2bd05dc2229a47565e66928ac7b63d2217170d0b
wanggang <wanggang@hryun.com>
2020-03-03 17:23:08 +0800

    set AI for first

--- src/global/init.js
+++ src/global/init.js
@@ -1,7 +1,7 @@
-import axios from 'gl/axios'
+import axios from "gl/axios";
 
 axios.post("/login", {
-    client: "demo12",
-    username: "00001",
-    password: "000000"
-})
+  client: "001",
+  username: "0363",
+  password: "087373"
+});
