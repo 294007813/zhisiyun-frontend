1644d0ff35a7e4de33f7abb137a6247c1e21592c
root <root@.localdomain>
2020-03-03 17:30:25 +0800

    init

--- src/global/init.js
+++ src/global/init.js
@@ -1,15 +1,7 @@
 import axios from "gl/axios";
 
-<<<<<<< HEAD
 // axios.post("/login", {
 //     client: "demo12",
 //     username: "00001",
 //     password: "000000"
 // })
-=======
-axios.post("/login", {
-  client: "demo",
-  username: "00001",
-  password: "000000"
-});
->>>>>>> master
