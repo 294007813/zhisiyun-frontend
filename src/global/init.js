4dcae8a6c750a0448b69f2f6684b39bc9fc7d7a0
root <root@.localdomain>
2020-03-04 18:37:02 +0800

    init

--- src/global/init.js
+++ src/global/init.js
@@ -1,27 +1,8 @@
 import axios from "gl/axios";
 import store from 'st'
 
-<<<<<<< HEAD
 // axios.post("/login", {
 //     client: "demo12",
 //     username: "00001",
 //     password: "000000"
 // })
-=======
-axios.post("/login", {
-  client: "demo12",
-  username: "00001",
-  password: "000000"
-
-  // client: "001",
-  // username: "0062",
-  // password: "000000"
-});
-
-//获取显示语言
-axios.get("/api/feishu/base/getlanguage").then(data=>{
-  if(data){
-    store.commit('setLang', data);
-  }
-});
->>>>>>> master
