c92ab85277ed5aa76a326b096ce2e290c503d3d4
qiang <qiang@qiangdeMacBook.local>
2020-03-05 14:09:50 +0800

    3.5

--- src/pages/home-config/staff-config.vue
+++ src/pages/home-config/staff-config.vue
@@ -3,7 +3,7 @@
     <div class="head">
         <h2>员工首页PC端布局及设置</h2>
         <div class="button">
-            <el-button size="small" @click="back">后退</el-button>
+            <el-button size="small" @click="back">返回</el-button>
             <el-button type="primary" size="small" @click="save">保存</el-button>
         </div>
     </div>
