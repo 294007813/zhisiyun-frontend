a976171804a67d8ec0b091702b1e2e733dd82f73
qiang <qiang@qiangdeMacBook.local>
2020-03-06 16:38:49 +0800

    3.6

--- src/pages/home-config/config/pc-items-key.js
+++ src/pages/home-config/config/pc-items-key.js
@@ -1,93 +1,90 @@
 export default {
-    modules:{
-        "basic_information_module":{
-            "default": "员工状态",
-            "status": "员工状态",
-            "level": "职级、层级、等级",
-            "sub": "下属",
-            "look": "查看档案",
-            "medal": "勋章"
-        },
-        "attendance_information_module":{
-            "attendance_statistics_month": "",
-            "holiday_balance": "",
-            "attendance_process": "",
-            "checkin": "出勤",
-            "late": "迟到",
-            "rest": "休息",
-            "early": "早退",
-            "miss": "缺卡",
-            "outside": "外勤",
-            "absent": "旷工",
-            "short": "缺勤",
-            "trip": "出差",
-            "overtime": "加班",
-            "official": "公干",
-            "leave": "请假",
-            "year": "年假余额",
-            "restbalance": "调休假余额",
-            "abnormal": "考勤异常",
-            "adjust": "假期调整"
-        },
-        "salary_information":{
-            monthly: "",
-            trend: "",
-            statistics: "",
-            "p_all_income": "累计收入",
-            "sum_tax": "累计个税",
-            "c_sb_total": "公司缴纳社保总数",
-            "p_sb_total": "个人缴纳社保总数",
-            "c_gjj_total": "公司缴纳公积金总数",
-            "p_gjj_total": "个人缴纳公积金总数",
-            "dikou_total": "抵扣总额"
-        },
-       "to_do_module":{
-           to_do_task:"",
-           done_task:"",
-       },
-        "performance_information_module":{
-            "current":"",
-            "trend":"",
-            "enter": "进入绩效首页"
-        },
-        "message_dynamic_module":{
-            unread: "",
-            readed: "",
-            "read": "全部设为已读"
-        },
-        "birthday_blessing_module":{
-            today: "",
-            this_month: "",
-        },
-        "company_star_module":{
-            "query": "搜索"
-        },
-        "skill_star_module":{
-            "query": "搜索",
-            "like": "点赞"
-        },
-        "training_process_module":{
-            train: "培训",
-            process: "流程",
-            "participate": "参与培训",
-            "doing": "进行中的活动",
-            "start": "我发起的",
-            "handle": "我处理的",
-            "doing2": "进行中的"
-        },
-        "contract_agreement_module":{
-            contract: "合同",
-            agreement: "协议"
-        },
-        "suggestion_box_module":{
-
-        },
-        "common_application_module":{
-
-        },
-        "my_calendar_module":{
-
-        },
+    "basic_information_module":{
+        "default": "员工状态",
+        "status": "员工状态",
+        "level": "职级、层级、等级",
+        "sub": "下属",
+        "look": "查看档案",
+        "medal": "勋章"
     },
+    "attendance_information_module":{
+        "attendance_statistics_month": "",
+        "holiday_balance": "",
+        "attendance_process": "",
+        "checkin": "出勤",
+        "late": "迟到",
+        "rest": "休息",
+        "early": "早退",
+        "miss": "缺卡",
+        "outside": "外勤",
+        "absent": "旷工",
+        "short": "缺勤",
+        "trip": "出差",
+        "overtime": "加班",
+        "official": "公干",
+        "leave": "请假",
+        "year": "年假余额",
+        "restbalance": "调休假余额",
+        "abnormal": "考勤异常",
+        "adjust": "假期调整"
+    },
+    "salary_information":{
+        monthly: "",
+        trend: "",
+        statistics: "",
+        "p_all_income": "累计收入",
+        "sum_tax": "累计个税",
+        "c_sb_total": "公司缴纳社保总数",
+        "p_sb_total": "个人缴纳社保总数",
+        "c_gjj_total": "公司缴纳公积金总数",
+        "p_gjj_total": "个人缴纳公积金总数",
+        "dikou_total": "抵扣总额"
+    },
+   "to_do_module":{
+       to_do_task:"",
+       done_task:"",
+   },
+    "performance_information_module":{
+        "current":"",
+        "trend":"",
+        "enter": "进入绩效首页"
+    },
+    "message_dynamic_module":{
+        unread: "",
+        readed: "",
+        "read": "全部设为已读"
+    },
+    "birthday_blessing_module":{
+        today: "",
+        this_month: "",
+    },
+    "company_star_module":{
+        "query": "搜索"
+    },
+    "skill_star_module":{
+        "query": "搜索",
+        "like": "点赞"
+    },
+    "training_process_module":{
+        train: "培训",
+        process: "流程",
+        "participate": "参与培训",
+        "doing": "进行中的活动",
+        "start": "我发起的",
+        "handle": "我处理的",
+        "doing2": "进行中的"
+    },
+    "contract_agreement_module":{
+        contract: "合同",
+        agreement: "协议"
+    },
+    "suggestion_box_module":{
 
+    },
+    "common_application_module":{
+
+    },
+    "my_calendar_module":{
+
+    },
 }
