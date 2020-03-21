477adb65d624ba07fe922cf3012245c91a75a243
maxuan <maxuan@gisiinfo.com>
2020-03-21 16:03:54 +0800

    en change

--- src/components/staff-select.vue
+++ src/components/staff-select.vue
@@ -6,7 +6,7 @@
     <div class="content">
         <el-button size="small" @click="toexpand(true)">{{$t('ptree.open')}}</el-button>
         <el-button size="small" style="margin-right: 10px" @click="toexpand(false)">{{$t('ptree.close')}}</el-button>
-        <el-input size="small" style="width: 300px" placeholder="{{$t('ptree.placeholder')}}"
+        <el-input size="small" style="width: 300px" :placeholder="$t('ptree.placeholder')"
                   v-model="value" @change="query">
             <el-button slot="append" icon="el-icon-search" @click="query"></el-button>
         </el-input>
